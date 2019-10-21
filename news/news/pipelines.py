836690f1dc37556b5300a7e4154bb5b102b591e3
Raphael Alencar <raphael.alencar.trd@c6bank.com>
2019-10-21 15:10:26 -0300

    remove mongo_provider

--- news/news/pipelines.py
+++ news/news/pipelines.py
@@ -1,29 +1,31 @@
 import pymongo
-from news.mongo_provider import MongoProvider
-
 
 class MongoPipeline(object):
 
-	def __init__(self, settings):
-		self.mongo_provider = MongoProvider(
-			settings.get('MONGO_URI'),
-			settings.get('MONGO_DATABSE')
-		)
+	collection_name = 'tc_news'
+
+	def __init__(self, mongo_uri, mongo_db):
+	    self.mongo_uri = mongo_uri
+	    self.mongo_db = mongo_db
 
 	@classmethod
 	def from_crawler(cls, crawler):
-		return cls(crawler.settings)
+	    return cls(
+	        mongo_uri=crawler.settings.get('MONGO_URI'),
+	        mongo_db=crawler.settings.get('MONGO_DATABASE', 'news')
+	    )
 
 	def open_spider(self, spider):
-		self.collection = self.mongo_provider.get_collection()
+	    self.client = pymongo.MongoClient(self.mongo_uri)
+	    self.db = self.client[self.mongo_db]
 
 	def close_spider(self, spider):
-		self.mongo_provider.close_connection()
+	    self.client.close()
 
 	def process_item(self, item, spider):
-		self.collection.find_one_and_update(
-			{"link": item["link"]},
-			{"$set": dict(item)},
-			upsert=True
+		self.db[self.collection_name].find_one_and_update(
+		    {"link": item["link"]},
+		    {"$set": dict(item)},
+		    upsert=True
 		)
 		return item
--- news/news/spiders/news.py
+++ news/news/spiders/news.py
@@ -3,34 +3,17 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.loader import ItemLoader
 from scrapy.utils.project import get_project_settings
 from news.items import TecmundoItem
-from news.mongo_provider import MongoProvider
 
 class TecmundoSpider(scrapy.Spider):
     name = 'Tecmundo'
     allowed_domains = ['tecmundo.com.br']
     start_urls = ['http://tecmundo.com.br/novidades']
 
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        kwargs['mongo_uri'] = crawler.settings.get('MONGO_URI')
-        kwargs['mongo_database'] = crawler.settings.get('MONGO_DATABASE')
-        return super(TecmundoSpider, cls).from_crawler(crawler, *args, **kwargs)
-
-    def __init__(self, limit_pages=None, mongo_uri=None, mongo_database=None, *args, **kwargs):
-        super(TecmundoSpider, self).__init__(*args, **kwargs)
-        self.mongo_provider = MongoProvider(mongo_uri, mongo_database)
-        self.collection = self.mongo_provider.get_collection()
-        last_items = self.collection.find().sort("date", -1).limit(1)
-        self.last_scraped_url = last_items[0]["link"] if last_items.count() else None
-
     def parse(self, response):
         for article in response.xpath('//*[@id="js-main"]//article//a[contains(@class, "tec--card__title__link")]/@href'):
             # Pegando dados de cada noticia específica
             link = article.get()
-            if link == self.last_scraped_url:
-                print("reached last item scraped, breaking loop")
-                return
-
+           
             yield response.follow(link, self.parse_article)
 
     def parse_article(self, response):
@@ -46,6 +29,11 @@ class TecmundoSpider(scrapy.Spider):
 
         return l.load_item()
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(TecmundoSpider)
-process.start()
+# class TecnoblogSpider(scrapy.Spider):
+# 	name = 'Tecnoblog'
+#     allowed_domains = ['tecnoblog.net']
+#     start_urls = ['http://tecnoblog.net/categoria/news']
+
+# process = CrawlerProcess(get_project_settings())
+# process.crawl(TecmundoSpider)
+# process.start()
