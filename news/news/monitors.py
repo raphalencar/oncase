171a987a30b9879f20f6bcd6064ae3274d90c15e
Raphael Alencar <raphalencar@gmail.com>
2019-10-23 02:33:34 -0300

    monitoring minimum items extracted

--- news/news/monitors.py
+++ news/news/monitors.py
@@ -6,7 +6,7 @@ class ItemCountMonitor(Monitor):
 	@monitors.name("Minimum number of items")
 	def test_minimum_number_of_items(self):
 		item_extracted = getattr(self.data.stats, "item_scraped_count", 0)
-		minimum_threshold = 10
+		minimum_threshold = 1000
 		msg = "Extracted less than {} items".format(minimum_threshold)
 		self.assertTrue(item_extracted >= minimum_threshold, msg=msg)
 
